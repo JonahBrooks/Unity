52c3eae7d626e5f1242db105ca890da582fca527
JonahBrooks <brooks.jonah.c@gmail.com>
2016-12-12 21:07:54 -0800

    Initial commit. Includes completed Roller Ball tutorial with added gear VR support.

---
+++ RollerBall/Roller Ball Tutorial/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class CameraScript : MonoBehaviour {
+
+    public GameObject player;
+
+    //private Vector3 offset;
+    //private Vector3 rotate;
+	
+    // Use this for initialization
+	void Start () {
+        //offset = transform.position - player.transform.position;
+    }
+
+
+    private void FixedUpdate()
+    {
+        //float moveHorizontal = Input.GetAxis("Horizontal");
+        //float moveVertical = Input.GetAxis("Vertical");
+        
+    }
+
+    // LateUpdate is called after every other update
+    void LateUpdate () {
+        //transform.position = player.transform.position + offset;
+	}
+}
