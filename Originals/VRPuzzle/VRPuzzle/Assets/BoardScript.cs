e31308fbad4fc129bb2b17dc3526f17ae79eff86
JonahBrooks <brooks.jonah.c@gmail.com>
2016-12-21 14:50:17 -0800

    Initial commit for VRPuzzle. Lots left to do.

---
+++ Originals/VRPuzzle/VRPuzzle/Assets/BoardScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardScript : MonoBehaviour {
+
+    // Matrix for storing pieces
+    private bool[,] board = new bool[16,16];
+
+    // TODO: Function for trying to set a piece: Return success or fail. Place piece if success.
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 16; i++)
+        {
+            for (int j = 0; j < 16; j++)
+            {
+                board[i,j] = false; // Set board to empty
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Originals/VRPuzzle/VRPuzzle/Assets/PlayerScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    // To hold the brick currently currently being moved, if any.
+    public GameObject current;
+    // To hold nearest game board (Currently only game board)
+    public GameObject board;
+
+    // To hold the current rotation (in degrees) of the current brick.
+    private float rotation;
+
+    // For use in changing camera angles
+    // TODO: Add camera movement. Right drag for camera move.
+
+	// Use this for initialization
+	void Start () {
+
+        current = null;
+        rotation = 0.0f;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// TODO: Get left click information.
+        // TODO: Raycast to find object to pick up if no object currently, drop if so.
+        // TODO: Hold that object into current.
+        // TODO: Get middle mouse wheel, rotate brick.
+        // TODO: Get middle click, rotate brick by 90 degree chunks.
+
+        // TODO: Detect proximity to board.
+        // TODO: Left click becomes place at 90 degree snaps if possible, buzz if not.
+        // TODO: Call board functions to place piece.
+	}
+}
