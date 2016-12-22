5ebcd7b9de5f84c5c26ec7184beecfa3ef8fca4c
JonahBrooks <brooks.jonah.c@gmail.com>
2016-12-22 08:30:29 -0800

    Board proximity detection in VRPuzzle now works.

--- Originals/VRPuzzle/VRPuzzle/Assets/PlayerScript.cs
+++ Originals/VRPuzzle/VRPuzzle/Assets/PlayerScript.cs
@@ -10,6 +10,8 @@ public class PlayerScript : MonoBehaviour {
     public float rotationSpeed;
     // To hold nearest game board (Currently only game board)
     public GameObject board;
+    // Distance at which a piece will snap into place if dropped
+    public float snapDistance;
     // For debugging
     public Text debug;
 
@@ -37,6 +39,13 @@ public class PlayerScript : MonoBehaviour {
 	void Update () {
         RaycastHit hit;
         Ray ray;
+        // Positions
+        Vector3 boardXYZ;
+        Vector3 brickXYZ;
+        // Height, width, length
+        Vector3 boardHWL;
+        Vector3 brickHWL;
+
 		// Get left click information.
         if (Input.GetMouseButtonDown(0))
         {
@@ -76,17 +85,38 @@ public class PlayerScript : MonoBehaviour {
                 rotation = rotation % 360;
             }
         }
-        current.transform.Rotate(new Vector3(0, 1, 0), Input.GetAxis("Mouse ScrollWheel") * rotationSpeed);
         if (current != null)
         {
+            current.transform.Rotate(new Vector3(0, 1, 0), Input.GetAxis("Mouse ScrollWheel") * rotationSpeed);
             delta = new Vector3(Input.mousePosition.x,0, Input.mousePosition.y)*mouseSpeed - last;
             last = new Vector3(Input.mousePosition.x, 0, Input.mousePosition.y)*mouseSpeed;
 
             // TODO: Use Time.deltaTime
             current.transform.Translate(delta, Space.World);
-            debug.text = delta.ToString();
         }
         // TODO: Detect proximity to board.
+            // TODO: Calculate distance using just the X and Z coordinates of board and current
+        if (current != null)
+        {
+            // TODO: Remove these unused variables
+            boardXYZ = board.GetComponentInChildren<Renderer>().bounds.center;
+            brickXYZ = current.GetComponentInChildren<Renderer>().bounds.center;
+            boardHWL = board.GetComponentInChildren<Renderer>().bounds.size;
+            brickHWL = current.GetComponentInChildren<Renderer>().bounds.size;
+            debug.text = Vector3.Distance(brickXYZ,boardXYZ).ToString();
+
+            // Check to see if brick is above and close enough to board
+            if(brickXYZ.y - boardXYZ.y < snapDistance &&
+                brickXYZ.y - boardXYZ.y > 0 &&
+                brickXYZ.x + brickHWL.x / 2.0f > boardXYZ.x - boardHWL.x / 2.0f &&
+                brickXYZ.x - brickHWL.x / 2.0f < boardXYZ.x + boardHWL.x / 2.0f &&
+                brickXYZ.z + brickHWL.z / 2.0f > boardXYZ.z - boardHWL.z / 2.0f &&
+                brickXYZ.z - brickHWL.z / 2.0f < boardXYZ.z + boardHWL.z / 2.0f
+              )
+            {
+                // Debug: current.transform.Rotate(new Vector3(1, 0, 0), 90);
+            }
+        }
         // TODO: Left click becomes place at 90 degree snaps if possible, buzz if not.
         // TODO: Call board functions to place piece.
     }
