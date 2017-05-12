ac4a044e8ba59ef5c144563c98ef9e4a4d739e73
Jonah Brooks <Brooks.Jonah.C@gmail.com>
2017-05-12 11:43:20 -0700

    Platformer attacking and gliding now animate. Gliding now functions.

--- Originals/Platformer/Platformer/Assets/Scripts/PlayerController.cs
+++ Originals/Platformer/Platformer/Assets/Scripts/PlayerController.cs
@@ -11,14 +11,19 @@ public class PlayerController : MonoBehaviour {
     public Transform groundCheck;
     public bool grounded;
     public float minJumpDuration = 0.1f;
+    public int hp = 10;
 
     private Rigidbody2D rb2d;
     private Animator anim;
     private SpriteRenderer sprite;
 
     private float jumpTime;
-    private bool jumped;
-    
+    private bool isJumping = false;
+    private bool facingRight = true;
+    private bool isAttacking = false;
+    private bool isThrowing = false;
+    private bool isDead = false;
+    private bool isGliding = false;
 
 	// Use this for initialization
 	void Start () {
@@ -34,36 +39,51 @@ public class PlayerController : MonoBehaviour {
 
     void FixedUpdate()
     {
+        isAttacking = anim.GetCurrentAnimatorStateInfo(0).IsName("NinjaGirlAttack")
+            || anim.GetCurrentAnimatorStateInfo(0).IsName("NinjaGirlJumpAttack");
+        isThrowing = anim.GetCurrentAnimatorStateInfo(0).IsName("NinjaGirlThrow")
+            || anim.GetCurrentAnimatorStateInfo(0).IsName("NinjaGirlJumpThrow");
+
         // Update grounded to see if player is on the ground
         grounded = Physics2D.Linecast(gameObject.transform.position,
                                       groundCheck.position,
                                       1 << LayerMask.NameToLayer("Ground"));
 
+        if(hp <= 0)
+        {
+            if (!isDead)
+            {
+                anim.SetTrigger("ninjaGirlDead");
+                isDead = true;
+            }
+        }
+
         // Jump
-        if (Input.GetButton("Jump") && grounded)
+        if (!isDead && Input.GetButtonDown("Jump") && grounded)
         {
             anim.SetTrigger("ninjaGirlJump");
             jumpTime = minJumpDuration;
-            jumped = true;
+            isJumping = true;
             rb2d.AddForce((Vector2.up * jumpPower));
         }
         // Count down min jump time
-        if(jumpTime > 0 && jumped)
+        if(jumpTime > 0 && isJumping)
         {
             jumpTime -= Time.deltaTime;
         }
         // Land from anything in the air
         // NOTE: This is buggy and will sometimes prevent the jump animation
-        if (grounded && jumpTime <= 0 && jumped)
+        if (!isDead && grounded && jumpTime <= 0 && isJumping)
         {
             anim.SetTrigger("ninjaGirlLand");
-            jumped = false;
+            isJumping = false;
+            isGliding = false;
         }
 
 
         // Control left and right movement
         float horizontal = Input.GetAxis("Horizontal");
-        if(Mathf.Abs(horizontal) > float.Epsilon)
+        if(!isDead && !isThrowing && !isAttacking && Mathf.Abs(horizontal) > float.Epsilon)
         {
             // Don't play run animation in the air
             if(grounded)
@@ -78,15 +98,55 @@ public class PlayerController : MonoBehaviour {
         }
 
 
-
-        // Flip sprite if moving left
-        if (horizontal < 0)
+        // Attack 
+        if(!isDead && Input.GetButtonDown("Fire1"))
         {
-            sprite.flipX = true;
+           if(!isAttacking && !isThrowing)
+           {
+                if(grounded)
+                {
+                    anim.SetTrigger("ninjaGirlAttack");
+                }
+                else
+                {
+                    anim.SetTrigger("ninjaGirlJumpAttack");
+                }    
+           }
+        }
+
+
+        // Glide
+        if(isJumping && !isDead && !isAttacking && !isThrowing && Input.GetButton("Fire3"))
+        {
+            isGliding = true;
+            anim.SetTrigger("ninjaGirlGlide");
         }
         else
         {
+            isGliding = false;
+            anim.SetTrigger("ninjaGirlStopGlide");
+        }
+        
+        // If gliding down
+        if(isGliding && rb2d.velocity.y < 0)
+        {
+            rb2d.gravityScale = 0.2f;
+        }
+        else
+        {
+            rb2d.gravityScale = 1f;
+        }
+
+        // Flip sprite if moving left
+        if (!isDead && !isAttacking && !isThrowing && horizontal < 0)
+        {
+            sprite.flipX = true;
+            facingRight = false;
+        }
+        else if (!isDead && !isAttacking && !isThrowing && horizontal > 0)
+        {
             sprite.flipX = false;
+            facingRight = true;
         }
 
         // Implement speed limit
