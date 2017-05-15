aa16ddecae8cfc2365ed5c28f5dcf8aba250f682
Jonah Brooks <Brooks.Jonah.C@gmail.com>
2017-05-14 19:23:14 -0700

    Initial commit for GobbleUp (based on the RollerBallTutorial). Also modified the level in which Platformer takes place.

---
+++ Originals/GobbleUp/GobbleUp/Assets/Scripts/manual_animator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class manual_animator : MonoBehaviour {
+
+    public GameObject[] frames;
+    public double frameTime = .01;
+
+    private double timer;
+    private int activeIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+        timer = frameTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Count to see when the next frame of animation needs to be played
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            timer = frameTime;
+            // Advance the frame of animation
+            activeIndex++;
+            activeIndex = activeIndex % frames.Length;
+        }
+        for (int i = 0; i < frames.Length; i++)
+        {
+            if (i != activeIndex)
+            {
+                frames[i].SetActive(false);
+            }
+            else
+            {
+                frames[i].SetActive(true);
+            }
+        }
+    }
+}
