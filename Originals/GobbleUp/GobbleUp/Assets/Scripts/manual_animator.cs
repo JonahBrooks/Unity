5455db464aaa206ff73ed6e266c58081bd19dc7b
Jonah Brooks <Brooks.Jonah.C@gmail.com>
2017-05-14 19:48:02 -0700

    GobbleUp now has a variable animation speed based on velocity of the duck.

--- Originals/GobbleUp/GobbleUp/Assets/Scripts/manual_animator.cs
+++ Originals/GobbleUp/GobbleUp/Assets/Scripts/manual_animator.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 public class manual_animator : MonoBehaviour {
 
     public GameObject[] frames;
-    public double frameTime = .01;
+    public double animationModifier = 1.0;
 
+    private double frameTime = .01;
     private double timer;
+    private double relativeTimer;
+    private Rigidbody rb;
     private int activeIndex = 0;
 
 	// Use this for initialization
 	void Start () {
+        relativeTimer = frameTime;
         timer = frameTime;
-	}
+        rb = GetComponent<Rigidbody>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        if(rb.velocity.sqrMagnitude > 0)
+        {
+            relativeTimer = animationModifier / rb.velocity.sqrMagnitude;
+        }
+        timer += relativeTimer - frameTime;
+        frameTime = relativeTimer;
         // Count to see when the next frame of animation needs to be played
         timer -= Time.deltaTime;
         if(timer <= 0)
