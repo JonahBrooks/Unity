34baafff80977bf320cc410b820869496d5a88e9
Jonah Brooks <Brooks.Jonah.C@gmail.com>
2019-08-25 19:18:28 -0700

    Updated LeapMotionBreakOut with a ball and other usefull things.

---
+++ Originals/LeapMotionBreakOut/LeapMotionBreakOut/Assets/Scripts/BallController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float ballSpeed;
+    public Vector3 initialTrajectory;
+
+    private Vector3 currentTrajectory;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTrajectory = initialTrajectory.normalized;
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = currentTrajectory * ballSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        // Don't allow the ball to move faster or slower than its given ball speed
+        rb.velocity = rb.velocity.normalized * ballSpeed;
+    }
+}
+
--- Originals/LeapMotionBreakOut/LeapMotionBreakOut/Assets/Scripts/PaddleController.cs
+++ Originals/LeapMotionBreakOut/LeapMotionBreakOut/Assets/Scripts/PaddleController.cs
@@ -8,12 +8,20 @@ public class PaddleController : MonoBehaviour
 
     public GameObject rightPaddle;
     public GameObject leftPaddle;
+    public bool rightPaddleTilt;
+    public bool leftPaddleTilt;
     public PaddleBounds rightPaddleBounds;
     public PaddleBounds leftPaddleBounds;
     public PaddleBounds rightLeapBounds;
     public PaddleBounds leftLeapBounds;
 
+    public float movementSpeed;
+
     private Controller controller;
+    private Rigidbody rightRb;
+    private Rigidbody leftRb;
+    private Vector3 rightNewPosition;
+    private Vector3 leftNewPosition;
 
     [System.Serializable]
     public class PaddleBounds
@@ -24,10 +32,18 @@ public class PaddleController : MonoBehaviour
         public float Bottom;
     }
 
+    private void Awake()
+    {
+        controller = new Controller();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        controller = new Controller();
+        rightRb = rightPaddle.GetComponent<Rigidbody>();
+        leftRb = leftPaddle.GetComponent<Rigidbody>();
+        rightNewPosition = rightPaddle.transform.position;
+        leftNewPosition = leftPaddle.transform.position;
     }
 
     // Update is called once per frame
@@ -36,24 +52,53 @@ public class PaddleController : MonoBehaviour
         // Get hands
         List<Hand> rawHands = controller.Frame().Hands;
 
-        Debug.Log(rawHands.Count);
+        //Debug.Log(rawHands.Count);
 
         foreach(Hand hand in rawHands)
         {
             // TODO: Find some way to ignore other people's hands
             if (hand.IsRight)
             {
-                MovePaddle(rightPaddle, hand, rightLeapBounds, rightPaddleBounds);
+                UpdatePaddleNewPosition(rightPaddle, hand, rightLeapBounds, rightPaddleBounds, out rightNewPosition);
+                // If the right paddle is allowed to tilt
+                if (rightPaddleTilt)
+                {
+                    TiltPaddle(rightPaddle, hand);
+                }
             }
             else if (hand.IsLeft)
             {
-                MovePaddle(leftPaddle, hand, leftLeapBounds, leftPaddleBounds);
+                UpdatePaddleNewPosition(leftPaddle, hand, leftLeapBounds, leftPaddleBounds, out leftNewPosition);
+                // If the left paddle is allowed to tilt
+                if (leftPaddleTilt)
+                {
+                    TiltPaddle(leftPaddle, hand);
+                }
             }
         }
     }
 
-    // Moves the specified paddle in relation to the specified hand and boundaries
-    private void MovePaddle(GameObject paddle, Hand hand, PaddleBounds leapBounds, PaddleBounds paddleBounds)
+    private void FixedUpdate()
+    {
+        if(rightRb.position != rightNewPosition)
+        {
+            rightRb.MovePosition(Vector3.MoveTowards(rightRb.position, rightNewPosition, movementSpeed * Time.deltaTime));
+        }
+        if (leftRb.position != leftNewPosition)
+        {
+            leftRb.MovePosition(Vector3.MoveTowards(leftRb.position, leftNewPosition, movementSpeed * Time.deltaTime));
+        }
+    }
+
+    private void TiltPaddle(GameObject paddle, Hand hand)
+    {
+        Vector3 newNormal = new Vector3(-hand.PalmNormal.x, -hand.PalmNormal.y, 0); // TODO: Did I do z right?
+
+        paddle.transform.rotation = Quaternion.LookRotation(Vector3.forward, newNormal);
+    }
+
+    // Updates the newX and newY for moving the specified paddle in relation to the specified hand and boundaries
+    private void UpdatePaddleNewPosition(GameObject paddle, Hand hand, PaddleBounds leapBounds, PaddleBounds paddleBounds, out Vector3 newPosition)
     {
         float newX = hand.PalmPosition.x;
         float newY = hand.PalmPosition.y;
@@ -80,10 +125,7 @@ public class PaddleController : MonoBehaviour
         newX = ((newX - leapBounds.Left) * (paddleBounds.Right - paddleBounds.Left)) / (leapBounds.Right - leapBounds.Left) + paddleBounds.Left;
         newY = ((newY - leapBounds.Bottom) * (paddleBounds.Top - paddleBounds.Bottom)) / (leapBounds.Top - leapBounds.Bottom) + paddleBounds.Bottom;
 
-        Vector3 newPosition = new Vector3(newX, newY, this.transform.position.z);
-
-        paddle.transform.position = newPosition;
-        
+        newPosition = new Vector3(newX, newY, this.transform.position.z);
     }
     
 }
