3eab41fc0179b66339cccb27df6589dd91f2dbea
Jonah Brooks <Brooks.Jonah.C@gmail.com>
2019-09-01 15:03:46 -0700

    Updated LeapMotionBreakOut with a couple other ways to handle Leap Motion input.

--- Originals/LeapMotionBreakOut/LeapMotionBreakOut/Assets/Scripts/PaddleController.cs
+++ Originals/LeapMotionBreakOut/LeapMotionBreakOut/Assets/Scripts/PaddleController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Leap;
+using Leap.Unity;
 
 public class PaddleController : MonoBehaviour
 {
@@ -10,18 +11,25 @@ public class PaddleController : MonoBehaviour
     public GameObject leftPaddle;
     public bool rightPaddleTilt;
     public bool leftPaddleTilt;
+    public MovementStyleEnum movementStyle;
     public PaddleBounds rightPaddleBounds;
     public PaddleBounds leftPaddleBounds;
     public PaddleBounds rightLeapBounds;
     public PaddleBounds leftLeapBounds;
+    public PaddleBounds rightLeapProviderBounds;
+    public PaddleBounds leftLeapProviderBounds;
 
     public float movementSpeed;
+    public float rotationSpeed;
 
     private Controller controller;
+    private LeapProvider leapProvider;
     private Rigidbody rightRb;
     private Rigidbody leftRb;
     private Vector3 rightNewPosition;
     private Vector3 leftNewPosition;
+    private Quaternion rightNewRotation;
+    private Quaternion leftNewRotation;
 
     [System.Serializable]
     public class PaddleBounds
@@ -30,6 +38,13 @@ public class PaddleController : MonoBehaviour
         public float Right;
         public float Top;
         public float Bottom;
+    }
+
+    public enum MovementStyleEnum
+    {
+        LeapProvider,
+        Controller,
+        OnFixedFrame
     }
 
     private void Awake()
@@ -44,32 +59,108 @@ public class PaddleController : MonoBehaviour
         leftRb = leftPaddle.GetComponent<Rigidbody>();
         rightNewPosition = rightPaddle.transform.position;
         leftNewPosition = leftPaddle.transform.position;
+        rightNewRotation = rightPaddle.transform.rotation;
+        leftNewRotation = leftPaddle.transform.rotation;
+        leapProvider = FindObjectOfType<LeapProvider>() as LeapProvider;
+        if (movementStyle == MovementStyleEnum.OnFixedFrame)
+        {
+            leapProvider.OnUpdateFrame -= OnUpdateFrame;
+            leapProvider.OnUpdateFrame += OnUpdateFrame;
+            leapProvider.OnFixedFrame -= OnFixedFrame;
+            leapProvider.OnFixedFrame += OnFixedFrame;
+        }
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    protected virtual void OnUpdateFrame(Frame frame)
+    {
+
+    }
+
+    protected virtual void OnFixedFrame(Frame frame)
+    {
+        foreach(Hand hand in frame.Hands)
+        {
+            if(hand.IsRight)
+            {
+                UpdatePaddleNewPosition(rightPaddle, hand, rightLeapProviderBounds, rightPaddleBounds, out rightNewPosition);
+                if (rightPaddleTilt)
+                {
+                    UpdatePaddleNewRotation(rightPaddle, hand, out rightNewRotation);
+                }
+            }
+            else if (hand.IsLeft)
+            {
+                UpdatePaddleNewPosition(leftPaddle, hand, leftLeapProviderBounds, leftPaddleBounds, out leftNewPosition);
+                if (leftPaddleTilt)
+                {
+                    UpdatePaddleNewRotation(leftPaddle, hand, out leftNewRotation);
+                }
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         // Get hands
-        List<Hand> rawHands = controller.Frame().Hands;
 
-        foreach(Hand hand in rawHands)
+        // Old style
+        if (movementStyle == MovementStyleEnum.Controller)
         {
-            if (hand.IsRight)
+            List<Hand> rawHands = controller.Frame().Hands;
+            foreach (Hand hand in rawHands)
             {
-                UpdatePaddleNewPosition(rightPaddle, hand, rightLeapBounds, rightPaddleBounds, out rightNewPosition);
-                // If the right paddle is allowed to tilt
-                if (rightPaddleTilt)
+                if (hand.IsRight)
                 {
-                    TiltPaddle(rightPaddle, hand);
+                    UpdatePaddleNewPosition(rightPaddle, hand, rightLeapBounds, rightPaddleBounds, out rightNewPosition);
+                    // If the right paddle is allowed to tilt
+                    if (rightPaddleTilt)
+                    {
+                        UpdatePaddleNewRotation(rightPaddle, hand, out rightNewRotation);
+                    }
+                }
+                else if (hand.IsLeft)
+                {
+                    UpdatePaddleNewPosition(leftPaddle, hand, leftLeapBounds, leftPaddleBounds, out leftNewPosition);
+                    // If the left paddle is allowed to tilt
+                    if (leftPaddleTilt)
+                    {
+                        UpdatePaddleNewRotation(leftPaddle, hand, out leftNewRotation);
+                    }
                 }
             }
-            else if (hand.IsLeft)
+        }
+
+        // New style
+        if (movementStyle == MovementStyleEnum.LeapProvider)
+        {
+            Frame frame = leapProvider.CurrentFrame;
+            List<Hand> rawHands = frame.Hands;
+
+            foreach (Hand hand in rawHands)
             {
-                UpdatePaddleNewPosition(leftPaddle, hand, leftLeapBounds, leftPaddleBounds, out leftNewPosition);
-                // If the left paddle is allowed to tilt
-                if (leftPaddleTilt)
+                if (hand.IsRight)
                 {
-                    TiltPaddle(leftPaddle, hand);
+                    UpdatePaddleNewPosition(rightPaddle, hand, rightLeapProviderBounds, rightPaddleBounds, out rightNewPosition);
+                    // If the right paddle is allowed to tilt
+                    if (rightPaddleTilt)
+                    {
+                        UpdatePaddleNewRotation(rightPaddle, hand, out rightNewRotation);
+                    }
+                }
+                else if (hand.IsLeft)
+                {
+                    UpdatePaddleNewPosition(leftPaddle, hand, leftLeapProviderBounds, leftPaddleBounds, out leftNewPosition);
+                    // If the left paddle is allowed to tilt
+                    if (leftPaddleTilt)
+                    {
+                        UpdatePaddleNewRotation(leftPaddle, hand, out leftNewRotation);
+                    }
                 }
             }
         }
@@ -78,22 +169,24 @@ public class PaddleController : MonoBehaviour
     private void FixedUpdate()
     {
         // Move the paddles into position if the players hands have moved
-        if(rightRb.position != rightNewPosition)
+        if(rightRb.position != rightNewPosition || rightRb.rotation != rightNewRotation)
         {
             rightRb.MovePosition(Vector3.MoveTowards(rightRb.position, rightNewPosition, movementSpeed * Time.deltaTime));
+            rightRb.MoveRotation(Quaternion.RotateTowards(rightRb.rotation, rightNewRotation, rotationSpeed * Time.deltaTime));
         }
-        if (leftRb.position != leftNewPosition)
+        if (leftRb.position != leftNewPosition || leftRb.rotation != leftNewRotation)
         {
             leftRb.MovePosition(Vector3.MoveTowards(leftRb.position, leftNewPosition, movementSpeed * Time.deltaTime));
+            leftRb.MoveRotation(Quaternion.RotateTowards(leftRb.rotation, leftNewRotation, rotationSpeed * Time.deltaTime));
         }
     }
 
     // Adjusts the tilt of the paddles based on hand tilt
-    private void TiltPaddle(GameObject paddle, Hand hand)
+    private void UpdatePaddleNewRotation(GameObject paddle, Hand hand, out Quaternion paddleNewRotation)
     {
         Vector3 newNormal = new Vector3(-hand.PalmNormal.x, -hand.PalmNormal.y, 0);
 
-        paddle.transform.rotation = Quaternion.LookRotation(Vector3.forward, newNormal);
+        paddleNewRotation = Quaternion.LookRotation(Vector3.forward, newNormal);
     }
 
     // Updates the newX and newY for moving the specified paddle in relation to the specified hand and boundaries
@@ -126,5 +219,5 @@ public class PaddleController : MonoBehaviour
 
         newPosition = new Vector3(newX, newY, this.transform.position.z);
     }
-    
+
 }
