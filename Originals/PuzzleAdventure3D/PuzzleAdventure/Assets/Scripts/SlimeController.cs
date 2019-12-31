53db643236ed8994fb64aa3287e10ed7ccbe1fe4
Jonah Brooks <Brooks.Jonah.C@gmail.com>
2019-12-30 18:24:04 -0800

    Updated Puzzle Adventure 3D to have animated slimes and frictionless walls.

--- Originals/PuzzleAdventure3D/PuzzleAdventure/Assets/Scripts/SlimeController.cs
+++ Originals/PuzzleAdventure3D/PuzzleAdventure/Assets/Scripts/SlimeController.cs
@@ -9,21 +9,49 @@ public class SlimeController : MonoBehaviour
     [HideInInspector]
     public int index;
 
+    public float minDelayBetweenAnimations;
+    public float maxDelayBetweenAnimations;
+
+    private float delayUntilAnimation; 
     private Transform playerTransform;
 
     // Start is called before the first frame update
     void Start()
     {
         playerTransform = GameObject.FindWithTag("Player").transform;
+        delayUntilAnimation = Random.Range(minDelayBetweenAnimations, maxDelayBetweenAnimations);
     }
     
     // Update is called once per frame
     void Update()
     {
+        // Look at the player character
         Vector3 target = playerTransform.position;
         target = new Vector3(target.x, 0, target.z);
         transform.LookAt(target);
-        transform.Rotate(new Vector3(-90, 0, 0));
+
+        // Trigger animations
+        Animator animator = transform.gameObject.GetComponent<Animator>();
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            // Count down until the next animation
+            delayUntilAnimation -= Time.deltaTime;
+            if (delayUntilAnimation <= 0)
+            {
+                // Randomly pick an animation to play
+                float randomSelection = Random.Range(0f, 1f);
+                if(randomSelection <= 0.5f)
+                {
+                    animator.SetTrigger("WiggleTrigger");
+                }
+                else
+                {
+                    animator.SetTrigger("JumpTrigger");
+                }
+                // Reset the clock between animations
+                delayUntilAnimation = Random.Range(minDelayBetweenAnimations, maxDelayBetweenAnimations);
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
